816ce7cd305b6de03f70a5e3c2e7d7f369845636
Brempong Appiah Dankwah <brempong.dankwah@amalitech.com>
2025-07-15 08:12:11 +0000

    Add AWS S3 module and provider configuration with necessary variables

--- iac/main.tf
+++ iac/main.tf
@@ -1,5 +1,5 @@
 module "aws_s3_bucket" {
-  source = "./iac/module/s3"
+  source = "./module/s3"
   bucket_name = var.bucket_name
   access_key_id = var.access_key_id
   secret_access_key = var.secret_access_key
