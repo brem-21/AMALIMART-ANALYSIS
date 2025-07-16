fedcb237bc0f45495ceac2db66201bd06f79bd7a
Brempong Appiah Dankwah <brempong.dankwah@amalitech.com>
2025-07-16 10:50:11 +0000

    Add RDS module with necessary variables and outputs; update main.tf and .gitignore

--- iac/module/rds/main.tf
+++ iac/module/rds/main.tf
@@ -1,13 +1,4 @@
-module "aws_s3_bucket" {
-  source = "./module/s3"
-  bucket_name = var.bucket_name
-  access_key_id = var.access_key_id
-  secret_access_key = var.secret_access_key
-  region = var.region
-}
-
-module "aws_db_instance" {
-  source = "./module/rds"
+resource "aws_db_instance" "amalimart" {
   allocated_storage = var.allocated_storage
   db_name = var.db_name
   engine = var.engine
@@ -16,4 +7,6 @@ module "aws_db_instance" {
   username = var.username
   password = var.password
   parameter_group_name = var.parameter_group_name
+  skip_final_snapshot = true
+  publicly_accessible = true
 }
--- iac/module/rds/variables.tf
+++ iac/module/rds/variables.tf
@@ -1,23 +1,3 @@
-variable "region" {
-  type = string
-  description = "Name of the region in AWS"
-}
-
-variable "access_key_id" {
-  type = string
-  description = "Access Key ID of IAM USER"
-}
-
-variable "secret_access_key" {
-    type = string
-    description = "Secret Access Key of IAM USER"
-}
-
-variable "bucket_name" {
-    type = string
-    description = "Name of the S3 bucket used for BLOB Storage"
-}
-
 variable "allocated_storage" {
   description = "Storage of the instance"
 }
